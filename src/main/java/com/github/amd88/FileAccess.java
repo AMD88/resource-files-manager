fd771dbfda9873ec9cb19c311d246a4de500608f
vbalalla <vbalalla@gmail.com>
2016-12-13 13:34:57 +0530

    Update: FileAccess file added.

---
+++ src/main/java/com/github/amd88/FileAccess.java
@@ -0,0 +1,24 @@
+package com.github.amd88;
+
+import java.io.File;
+
+/**
+ * Created by vibodha on 12/13/16.
+ */
+public class FileAccess {
+    private static FileAccess fileAccess = null;
+    private static String path;
+
+    public FileAccess(String filePath) {
+        path = filePath;
+    }
+
+    public static FileAccess getInstance(){
+        return fileAccess = new FileAccess(path);
+    }
+
+    public File getFile(){
+        return new File(path);
+    }
+
+}
--- src/main/java/com/github/amd88/Main.java
+++ src/main/java/com/github/amd88/Main.java
@@ -2,6 +2,6 @@ package com.github.amd88;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("hello world !!");
+        System.out.println(new FileAccess("/home/vibodha/Firefox_wallpaper.png").getFile().length());
     }
 }
