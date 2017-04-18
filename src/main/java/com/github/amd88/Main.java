e632354b3dcb81662f8fe220c5072a58f7462bfc
amd88 <amodadissanayake@gmail.com>
2017-04-19 01:51:14 +0530

    fix(Main): testing for image resizing

--- src/main/java/com/github/amd88/Main.java
+++ src/main/java/com/github/amd88/Main.java
@@ -1,14 +1,18 @@
 package com.github.amd88;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         //System.out.println(new FileAccess("/home/vibodha/Pictures/Wallpapers/ubuntu_os_white_orange_30925_1366x768.jpg").getFile().length());
         //giSystem.out.println(new FileAccess("/home/vibodha/Pictures/Wallpapers").getFolder().length());
 
         //System.out.println(new FileAccess("/home/vibodha/Firefox_wallpaper.png").getFile().length());
 
-        MemoryManagement memoryManagement = new MemoryManagement() {
-            public int syncSave() {
+        /*MemoryManagement memoryManagement = new MemoryManagement() {
+            public int asyncmemorymanagement() {
                 for (int i = 0; i<100; i++){
                     m = i;
                     System.out.println(m);
@@ -22,6 +26,13 @@ public class Main{
             }
         };
         memoryManagement.start();
-        memoryManagement.syncSave();
+        memoryManagement.asyncmemorymanagement();*/
+        FileProcessor fp = new FileProcessor();
+        File file = new File("/home/amoda/Pictures/14281584_1780085735594878_2084325646_n88.jpg");
+       // File file1 = new File();
+fp.resizeImage(file,700,7000);
+        //File outputfile = new File("/home/amoda/Pictures/image.jpg");
+        //ImageIO.write(fp.resizeImage(file,1000,7000), "jpg", outputfile);
+
     }
 }
